d18092411f733ee079b6e237c78d17f0c3970562
Akshay Shinde <s.yahska@gmail.com>
2021-04-20 12:48:39 -0700

    Added constants file to replace magic numbers

---
+++ src/utilities/constants.js
@@ -0,0 +1,12 @@
+//Preload images config
+export const preloadImagesNumber = 5
+
+export const maxDragDistance = 70
+
+export const cardOpacityOffset = 150
+
+//For future implementations
+export const colors = {
+    "FFD166": {"default":"FFD166", "Protanopia": "EBEA7F"},
+    "4D243D": {"default":"4D243D", "Protanopia": "3B3A36"},
+}
