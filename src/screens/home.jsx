be5d6fe3d9f7faaf6565a769c3cb85142969dbe6
Akshay Shinde <s.yahska@gmail.com>
2021-04-19 19:51:30 -0700

    fetching csv data

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from "react";
 const dataEndpoint = "http://take-home-wildlife.s3-website-us-west-2.amazonaws.com/data.csv"
 
 const Home = ()=>{
-
+    useEffect(()=>{
+        fetch(dataEndpoint).then(response=>response.text()).then((textResult)=>{
+            console.log(textResult)
+        })
+    },[])
     return (
         <div className="main-page">
             Flashcard App
